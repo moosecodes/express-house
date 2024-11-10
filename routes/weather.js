56eb89343f52a8adae6dee4f3d56d7563f4437bd
Moose Codes <moosecodes@gmail.com>
2024-11-10 17:23:05 -0600

    remove unnecessary routes

--- routes/weather.js
+++ routes/weather.js
@@ -4,25 +4,6 @@ import db from '../services/utils.js';
 
 const router = express.Router();
 
-/* GET apit routes. */
-router.get('/weather/current', (req, res, next) => {
-  db.query('SELECT * FROM expressdb.weather_readings ORDER BY created_at DESC LIMIT 1', (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(results);
-  });
-});
-
-router.get('/weather/recent', (req, res, next) => {
-  db.query('SELECT * FROM expressdb.weather_readings ORDER BY created_at DESC LIMIT 10000', (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(results);
-  });
-});
-
 const fetchWeatherData = async () => {
   console.log(new Date().toString());
   try {
@@ -55,6 +36,29 @@ const fetchWeatherData = async () => {
 };
 
 fetchWeatherData();
-setInterval(fetchWeatherData, process.env.WEATHER_FETCH_INTERVAL);
+setInterval(fetchWeatherData, process.env.ETCH_INTERVAL);
+
+/* GET apit routes. */
+router.get('/weather/current', (req, res, next) => {
+  db.query('SELECT * FROM expressdb.weather_readings ORDER BY created_at DESC LIMIT 1', (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+router.get('/weather/recent', (req, res, next) => {
+  db.query('SELECT * FROM expressdb.weather_readings ORDER BY created_at DESC LIMIT 10000', (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+router.get('/weather/test', (req, res, next) => {
+  res.json({ message: "testing weather endpoint" });
+});
 
 export default router;
